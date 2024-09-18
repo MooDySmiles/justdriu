e7028ec757ed8e934b23684eeb1489b4d24c33a1
MooDySmiles <18305301+MooDySmiles@users.noreply.github.com>
2024-09-18 12:50:53 +0200

    fix: center login text

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ import GoogleAuth from "./google";
 export default function Login() {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-y-400">
-      <mds-text typography={"h1"} variant={"title"}>
+      <mds-text class="text-center" typography={"h1"} variant={"title"}>
         Benvenuto, effettua l&apos;accesso
       </mds-text>
       <GoogleAuth></GoogleAuth>
