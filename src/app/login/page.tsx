012c5a750cfadcf4b45b32fd182fa4199dc075d3
MooDySmiles <18305301+MooDySmiles@users.noreply.github.com>
2024-09-18 12:11:23 +0200

    change: change login page content

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+
+import "@/utils/mds-components";
+
 import GoogleAuth from "./google";
 
 export default function Login() {
-  return (
+  return <div className="flex flex-col h-screen justify-center items-center gap-y-400">
+    <mds-text typography={"h1"} variant={"title"}>Benvenuto, effettua l&apos;accesso</mds-text>
     <GoogleAuth></GoogleAuth>
-  );
+  </div>;
 }
