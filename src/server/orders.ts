aaab7999c3bb55afe44ceaa2d74b457118fe7c04
MooDySmiles <18305301+MooDySmiles@users.noreply.github.com>
2024-09-24 14:49:50 +0200

    fix: add use server marker

--- src/server/orders.ts
+++ src/server/orders.ts
@@ -2,10 +2,12 @@ import "server-only";
 import { orders } from "@/utils/temp_orders";
 
 export async function getMyOrders() {
+  "use server";
   //TODO retrieve orders from Supabase
   return orders;
 }
 
 export async function saveOrder(formData: FormData) {
+  "use server";
   console.log("Recevied", formData);
 }
