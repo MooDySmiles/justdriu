09f5b349dd64d80a28e5d15ec4146a2d8893656c
MooDySmiles <18305301+MooDySmiles@users.noreply.github.com>
2024-10-01 11:21:35 +0200

    chore: add logic to get and save data from database (wip)

--- src/server/orders.ts
+++ src/server/orders.ts
@@ -1,10 +1,27 @@
 import "server-only";
 import { orders } from "@/utils/temp_orders";
 import { redirect } from "next/navigation";
+import { createClient } from "@utils/supabase/server";
+import { getUserProfile } from "@utils/supabase/api/user";
 
 export async function getMyOrders() {
   "use server";
-  //TODO retrieve orders from Supabase (should be ordered by last updated? or last insert?)
+
+  const supabase = createClient();
+
+  const { data: user } = await getUserProfile(supabase);
+
+  if (!user) throw new Error("Unauthorized");
+  
+  // const { data: orders, error } = await supabase
+  //   .from("order")
+  //   .select()
+  //   .eq("organizer", user.id)
+  //   .order("updated_at");
+
+  // if (error) throw new Error(error.message);
+
+  // return orders;
   return orders.sort((a, b) => parseInt(b.id) - parseInt(a.id));
 }
 
@@ -12,5 +29,39 @@ export async function saveOrder(formData: FormData) {
   "use server";
   console.log("Recevied", formData);
 
-  redirect("/dashboard")
+  const supabase = createClient();
+
+  const { data: user } = await getUserProfile(supabase);
+
+  if (!user) throw new Error("Unauthorized");
+
+  const organizer = user.id;
+  const delivery_datetime = new Date(
+    `${formData.get("orderDate") as string} ${formData.get("orderTime") as string}`,
+  ).toISOString();
+  const end_hour = new Date(
+    `${formData.get("orderDate") as string} ${formData.get("endHour") as string}`,
+  ).toISOString();
+  const createdUpdatedAt = new Date().toISOString();
+
+  console.log(
+    "Organizer",
+    user,
+    organizer,
+    createdUpdatedAt,
+    delivery_datetime,
+    end_hour,
+  );
+
+  // const { error } = await supabase.from("order").insert({
+  //   created_at: createdUpdatedAt,
+  //   updated_at: createdUpdatedAt,
+  //   delivery_datetime,
+  //   end_hour,
+  //   organizer,
+  // });
+
+  // if (error) throw new Error(error.message);
+
+  redirect("/dashboard");
 }
