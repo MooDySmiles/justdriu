a916664c638541905e121273c15ccd8c4e83c5c0
Nicola Tamburini <nicola.tamburini@maggioli.it>
2024-10-03 11:27:58 +0200

    chore: remove comments

--- seed.ts
+++ seed.ts
@@ -43,19 +43,6 @@ const main = async () => {
       user_name: profile.username,
     })) ?? [];
 
-  // Seed the database with 3 auth_users
-  // await seed.auth_users((x) => x(3));
-  // await seed.profile(x => x(3));
-
-  // utente di test test@test.it pass: test DO NOT CHANGE
-  //   console.log(`
-  // insert into auth.users (instance_id, id, aud, role, email, encrypted_password, raw_app_meta_data, raw_user_meta_data, email_confirmed_at, created_at)
-  // values ('00000000-0000-0000-0000-000000000000', '6ab4b401-d9ee-4c96-a972-c44d542cd32c', 'authenticated', 'authenticated', 'test@test.it', '$2a$10$OctB9kNcQUy2W4vofoFjKOYtq4beq31dujrTWkIIZuZFje/2LEWX6', '{"provider":"email","providers":["email"]}', '{}', timezone('utc'::text, now()), timezone('utc'::text, now()));
-
-  // insert into auth.identities (id, user_id, provider_id, identity_data, provider, created_at)
-  // values ('6ab4b401-d9ee-4c96-a972-c44d542cd32c', '6ab4b401-d9ee-4c96-a972-c44d542cd32c', '6ab4b401-d9ee-4c96-a972-c44d542cd32c',	'{"sub": "6ab4b401-d9ee-4c96-a972-c44d542cd32c"}', 'email', timezone('utc'::text, now()));
-  // `)
-  // await seed.profile((x) => x(profile.length), {connect: {profile}})
   const { food_provider } = await seed.food_provider((x) => x(2));
   const { dish_type } = await seed.dish_type((x) => x(5));
   const { dish } = await seed.dish((x) => x(10), {
