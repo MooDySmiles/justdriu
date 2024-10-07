a1ae6e13cf2b0567a72d0f3389b5f868481f7bf1
Nicola Tamburini <nicola.tamburini@maggioli.it>
2024-10-07 12:39:23 +0200

    feat: add justdriu colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { type Config } from "tailwindcss";
 
 export default {
@@ -9,5 +10,23 @@ export default {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     require("@maggioli-design-system/styles"),
   ],
+  theme: {
+    extend: {
+      colors: {
+        // add custom justdriu colors
+        'justdriu-01': 'rgb(var(--justdriu-01))',
+        'justdriu-02': 'rgb(var(--justdriu-02))',
+        'justdriu-03': 'rgb(var(--justdriu-03))',
+        'justdriu-04': 'rgb(var(--justdriu-04))',
+        'justdriu-05': 'rgb(var(--justdriu-05))',
+        'justdriu-06': 'rgb(var(--justdriu-06))',
+        'justdriu-07': 'rgb(var(--justdriu-07))',
+        'justdriu-08': 'rgb(var(--justdriu-08))',
+        'justdriu-09': 'rgb(var(--justdriu-09))',
+        'justdriu-10': 'rgb(var(--justdriu-10))',
+        'justdriu': 'rgb(var(--justdriu))',  
+      }
+    }
+  },
   plugins: [],
 } satisfies Config;
